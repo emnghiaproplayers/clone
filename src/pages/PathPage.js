2d6dcae200feb7e37cf9abe802ee86e23b2b6dcc
emnghiaproplayers <truongnghia297@gmail.com>
2024-03-14 23:06:56 +0700

    add all

---
+++ src/pages/PathPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { dataCourse } from '../../src/data/dataCourse';
+import { Link } from "react-router-dom";
+
+function PathPage() {
+  const {course} = dataCourse
+  return <div>
+  <ul role="list">
+  {course.map((item, index) =>{
+      const {id, name, image} = item
+      return (
+        <li key={id}>
+          <Link to={`/course/${id}`}>
+            <h1>{name}</h1>
+            <img src={image.type} alt="" />
+          </Link>
+        </li>
+      )
+    })}
+  </ul>
+   
+  </div>;
+}
+
+export default PathPage;
